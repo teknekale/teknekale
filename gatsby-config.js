63fdf46539602f39a8c6d19917d668b8d830ecaa
simone.pachera <pachera.simone@gmail.com>
2017-08-12 19:14:32 +0100

    bootstrap

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`
+    title: 'Gatsby Default Starter'
   },
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [
+    {
+      resolve: 'gatsby-plugin-typography',
+      options: {
+        pathToConfigModule: 'src/utils/typography.js'
+      }
+    },
+    'gatsby-plugin-react-helmet'
+  ]
 };
